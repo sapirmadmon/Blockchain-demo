0588a581d0a8a8ca44060b2a4582f7cfadd1f8f3
harel <harelazimm1@gmail.com>
2021-06-18 12:25:43 +0300

    add block

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,14 +2,11 @@ import "./App.module.css";
 import React, { useEffect } from "react";
 import axios from "axios";
 import Navbar from "./router/Navbar";
-import Block from "./blockchain/block";
+import Block from "./blockchain/block.jsx";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Hash from "./hash/hash";
 
 function App() {
-  //const [apiData, setApiData] = useState([]);
-  //const [cityNames, setCityNames] = useState([]);
-
   const callAPI = () => {
     axios
       .get("http://localhost:3030/getAll")
