5a1dbc807427455af9b1aa1c26ec2aadaaaa318b
sapir <sapir.madmon@s.afeka.ac.il>
2021-07-18 15:21:15 +0300

    add init transaction

---
+++ server/routes/transaction.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const router = express.Router();
+const Signature = require("../public-private-keys/signature");
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
+const signatureContent = require("../model/signatureSchema");
+const updateSign = require("../DB/updateSignature");
+const id = "transaction";
+const SHA256 = require("crypto-js/sha256");
+
+router.get("/transaction/initTransaction", async(req, res) => {
+    const key = ec.genKeyPair();
+    //const key2 = ec.genKeyPair();
+
+    const privateKey = key.getPrivate("hex");
+    const fromPublicKey = ec
+        .keyFromPrivate(privateKey)
+        .getPublic("hex")
+        .toString();
+
+    const toPublicKey = ec.genKeyPair().getPublic("hex");
+    const message = ["20", fromPublicKey, toPublicKey];
+
+    const signature = new Signature(
+        JSON.stringify(message),
+        privateKey,
+        fromPublicKey,
+        ""
+    );
+
+    const content = new signatureContent({
+        ...signature,
+        id: id,
+    });
+    content.save().catch(() => {
+        updateSign(signature, id);
+    });
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.json({
+        message: message,
+        prKey: signature.prKey,
+        //puKey: signature.puKey,
+    });
+});
+
+router.post("/transaction/sign", async(req, res) => {
+    const message = req.body.message;
+    const privateKey = req.body.prKey;
+    const publicKey = ec.keyFromPrivate(privateKey).getPublic("hex").toString();
+
+    const hashMsg = SHA256(message).toString();
+    const messageSign = ec
+        .keyFromPrivate(privateKey)
+        .sign(hashMsg, "base64")
+        .toDER("hex");
+
+    const signature = new Signature(message, privateKey, publicKey, messageSign);
+
+    updateSign(signature, id);
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.json({
+        message: message,
+        puKey: publicKey,
+        signature: messageSign,
+    });
+});
+
+router.post("/transaction/verify", async(req, res) => {
+    const message = req.body.message;
+    const publicKey = req.body.puKey;
+    const messageSign = req.body.signature;
+    let ifVerify;
+    try {
+        const hashMsg = SHA256(message).toString();
+
+        ifVerify = ec.keyFromPublic(publicKey, "hex").verify(hashMsg, messageSign);
+    } catch (error) {
+        ifVerify = false;
+    }
+    res.header("Access-Control-Allow-Origin", "*");
+    res.json({
+        ifVerify: ifVerify,
+        message: message,
+        puKey: publicKey,
+        signature: messageSign,
+    });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -14,12 +14,13 @@ const routerBlock = require("./routes/block");
 const routerBlockchain = require("./routes/blockchain");
 const routerKeys = require("./routes/keys");
 const routerSignature = require("./routes/signature");
+const routerTransaction = require("./routes/transaction");
 
 app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb", extended: true }));
 app.use(
-  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+    express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
 );
 //routers
 app.use((req, res, next) => next(), routerHash);
@@ -27,24 +28,24 @@ app.use((req, res, next) => next(), routerBlock);
 app.use((req, res, next) => next(), routerBlockchain);
 app.use((req, res, next) => next(), routerKeys);
 app.use((req, res, next) => next(), routerSignature);
+app.use((req, res, next) => next(), routerTransaction);
 // db config
 mongoose.connect(
-  process.env.DATABASE_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-  () => {
-    console.log("connect to db...");
-  }
+    process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+    () => {
+        console.log("connect to db...");
+    }
 );
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 // db init with json file
 db.once("open", async function callback() {
-  await websiteContent.deleteMany();
-  await blockContent.deleteMany();
-  await blockchainContent.deleteMany();
-  await signatureContent.deleteMany();
+    await websiteContent.deleteMany();
+    await blockContent.deleteMany();
+    await blockchainContent.deleteMany();
+    await signatureContent.deleteMany();
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+    console.log(`Server running on port ${process.env.PORT}`);
 });
